b1884e160cff7f1558b1af127213c47e50e47348
BuildTools <unconfigured@null.spigotmc.org>
2023-05-24 22:32:15 -0700

    Nothing Made Yet... Besides This Entire Repository

---
+++ src/main/java/net/blasted246/sizechanger/SizeChanger.java
@@ -0,0 +1,15 @@
+package net.blasted246.sizechanger;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SizeChanger implements ModInitializer {
+	public static final String MOD_ID = "sizechanger";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/blasted246/sizechanger/SizeChangerClient.java
@@ -0,0 +1,10 @@
+package net.blasted246.sizechanger;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SizeChangerClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
